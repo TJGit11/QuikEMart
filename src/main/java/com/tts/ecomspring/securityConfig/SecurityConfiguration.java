51f810a1875b8b915f79d2f9b8ae9baec47fa399
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-02-26 12:06:50 -0600

    update gitignore to exclude application.properties

--- src/main/java/com/tts/ecomspring/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/tts/ecomspring/securityConfig/SecurityConfiguration.java
@@ -25,14 +25,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/cart").authenticated()
-                .and().formLogin()
-                    .loginPage("/signin")
-                    .loginProcessingUrl("/login")
+        http.authorizeRequests()
+                .antMatchers("/console/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/about").permitAll()
+                .antMatchers("/main").permitAll()
+                .antMatchers("/product").permitAll()
+                .antMatchers("/signin").permitAll()
+                .antMatchers("/custom.js").permitAll()
+                .antMatchers("/custom.css").permitAll()
+                .antMatchers("/cart").authenticated()
+                .antMatchers().hasAuthority("USER").anyRequest().authenticated()
+                .and().csrf().disable().formLogin()
+                .loginPage("/signin").failureUrl("/signin?error=true")
+                .defaultSuccessUrl("/")
+                .loginProcessingUrl("/login")
                 .and().logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-                    .logoutSuccessUrl("/");
+                .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
+                .logoutSuccessUrl("/")
+                .and().exceptionHandling()
+        ;
+        http.headers().frameOptions().disable();
     }
+
+
 }
